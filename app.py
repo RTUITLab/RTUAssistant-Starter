8f0da37acd4767a169499afe8f6a0622e5f1588a
Dmitry <dim0n2023@yandex.ru>
2020-04-30 05:16:46 +0300

    problems with TF

--- app.py
+++ app.py
@@ -1,20 +1,26 @@
-from flask import Flask, render_template
-from markupsafe import escape
+from flask import Flask, render_template, request, redirect
+import sounddevice as sd
+import numpy as np
+#from tensorflow import keras
+import scipy as sp
+import scipy.signal
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return 'Index page'
+@app.route('/index')
+def index(name='Hello world'):
+    name = request.args.get('name')
+    return render_template('main.html', name=name)
 
-@app.route('/<name>')
-def f(name):
-    return hello(name)
+@app.route('/request')
+def req():
+    request.args.add('name', 'req')
+    # request.args.add('home', '/')
+    # url = request.args.get('home')
+    return redirect('/')
 
-@app.route('/hello')
-def hello(name='new user'):
-    return render_template('hello.html', name=name)
 
-@app.route('/hello/')
-def about():
-    return 'The about page'
+
+
+
