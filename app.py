4a246b55645e1ad231dddf97db4f874100803286
Dmitry <dim0n2023@yandex.ru>
2020-05-01 13:30:02 +0300

    stream realised

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, make_response
 import sounddevice as sd
 import numpy as np
 #from tensorflow import keras
@@ -7,20 +7,32 @@ import scipy.signal
 
 app = Flask(__name__)
 
+sd.default.samplerate = 44100
+sd.default.channels = 1
+
+rec_duration = 0.1
+start_duration = 0.5
+
+i = 0
+
 @app.route('/')
 @app.route('/index')
-def index(name='Hello world'):
-    name = request.args.get('name')
-    return render_template('main.html', name=name)
+def index(time=0):
+    return render_template('main.html', time=time)
 
 @app.route('/request')
 def req():
-    request.args.add('name', 'req')
-    # request.args.add('home', '/')
-    # url = request.args.get('home')
-    return redirect('/')
+    global i
+    i += 1
+    return index(i)
 
+def callback(indata, outdata, frames, time, status):
+    
+    outdata[:] = indata
+    #return index()
 
-
+block = int(sd.default.samplerate * rec_duration)
+stream = sd.Stream(blocksize=block, callback=callback)
+stream.start()
 
 
